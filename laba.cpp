0361967b9d5889491fc36ec7396fbd74695bec27
Markova-A-S <nastjamarkova9@gmail.com>
2025-12-05 16:12:15 +0300

    Добавление исходных файлов проекта
    Загружено 5 файлов разных типов: png, pdf, docx, cpp, txt
    Файлы добавлены в основную ветку main.

---
+++ laba.cpp
@@ -0,0 +1,53 @@
+﻿#include <cstdbool>
+#include <iostream>
+using namespace std;
+
+bool number(char line[], char forbidden[]) {
+	bool count = true;
+	int n = strlen(line);
+	int d = strlen(forbidden);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < d; j++) {
+			if (line[i] == forbidden[j]) {
+				count = false;
+			}
+		}
+	}
+	return(count);
+}
+
+bool letter(char line[], char forbiddenLetter[]) {
+	bool character = true;
+	int n = strlen(line);
+	int d = strlen(forbiddenLetter);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < d; j++) {
+			if (line[i] == forbiddenLetter[j]) {
+				character = false;
+			}
+		}
+	}
+	return (character);
+}
+
+
+void check(char line[], char forbidden[],char forbiddenLetter[]) {
+	if (number(line, forbidden) && letter(line, forbiddenLetter)) {
+		cout << line << endl;
+	}
+	else {
+		cout << "" << endl;
+	}
+}
+char forbidden[5] = { '6', '7', '8', '9', '0' };
+char forbiddenLetter[7] = { 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+
+int main()
+{
+	while (true) {
+		char input[1000];
+		cin.getline(input, 1000);
+		check(input, forbidden, forbiddenLetter);
+	}
+}
+
